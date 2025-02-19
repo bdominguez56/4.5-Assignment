0a1dcab42b513cf01091277a4cee2b5b0c0a8341
bdominguez56 <111765600+bdominguez56@users.noreply.github.com>
2025-02-19 13:12:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+// to concatenate
+let name = "name1";
+let age = 25;
+let location = "New York";
+
+// cocncentrate using + operator
+let message = name + " is " + age + " years old and lives in " + location + ".";
+
+// or template literals
+let message = `${name} is ${age} years old and lives in ${location}.`;
+
+// output
+console.log(message);
 
